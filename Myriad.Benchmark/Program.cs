3d88e805bcbac8b78e35011fd1306a2d843f2746
Joel Ashford <joelcarolashford@hotmail.com>
2020-03-12 16:30:52 -0500

    Added benchmark project

---
+++ Myriad.Benchmark/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using System.Threading;
+
+namespace Myriad.Benchmark
+{
+    public class Benchmarker
+    {
+        [Benchmark]
+        public void Run()
+        {
+            Thread.Sleep(50);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarker>();
+        }
+    }
+}
