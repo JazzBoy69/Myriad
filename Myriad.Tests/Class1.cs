4926e91d01398a04efcc823d8c6b950448bfca58
Joel Ashford <joelcarolashford@hotmail.com>
2020-03-12 16:21:40 -0500

    Added test project

---
+++ Myriad.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Myriad;
+
+namespace Myriad.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 == 1);
+        }
+    }
+}
